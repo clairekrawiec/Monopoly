c6c7b9c4706b687aa19685665fa610a0583b2f35
Claire Krawiec <c.m.krawiec12@gmail.com>
2017-02-01 08:11:02 -0700

    2/1/2017

--- Monopoly/src/Character.java
+++ Monopoly/src/Character.java
@@ -1,10 +1,37 @@
-
+import java.util.*;
 public class Character {
 
 	private String name;
 	
 	
-	public Character(){
+	public Character(String n)
+	{
+		name = n;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public static void playerList()
+	{
+		ArrayList<Character> characterList = new ArrayList<Character>();
+		characterList.add(new Character("Top Hat"));
+		characterList.add(new Character("Thimble"));
+		characterList.add(new Character("Iron"));
+		characterList.add(new Character("Shoe"));
+		characterList.add(new Character("Battleship"));
+		characterList.add(new Character("Canon"));
+		characterList.add(new Character("Racecar"));
+		characterList.add(new Character("Purse"));
+		characterList.add(new Character("Rocking Horse"));
+		characterList.add(new Character("Scottish Dog"));
 		
 	}
 }
--- Monopoly/src/Property.java
+++ Monopoly/src/Property.java
@@ -1,7 +1,69 @@
+import java.util.*;
+public class Property 
+{
+	private String name;
+	private String color;
+	private int price;
+	private int rent;
+	private int houseNum;
+	private int housePrice;
+	private String owner;
+	private int position;
 
-public class Property {
-
-	public Property(){
+	public Property(String n, String c, int p,int r, int hN, int hP, String o, int pos)
+	{
+		name = n;
+		color = c;
+		rent = r;
+		price = p;
+		houseNum = hN;
+		housePrice = hP;
+		owner = o;
+		position = pos;
 		
 	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public int getRent() {
+		return rent;
+	}
+	public void setRent(int rent) {
+		this.rent = rent;
+	}
+	public int getHouseNum() {
+		return houseNum;
+	}
+	public void setHouseNum(int houseNum) {
+		this.houseNum = houseNum;
+	}
+	public int getHousePrice() {
+		return housePrice;
+	}
+	public void setHousePrice(int housePrice) {
+		this.housePrice = housePrice;
+	}
+	public String getOwner() {
+		return owner;
+	}
+	public void setOwner(String owner) {
+		this.owner = owner;
+	}
+
 }
+
